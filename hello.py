795dd52a4d0a5a1df747aecefe5fea2c537ae807
SVenose <salvenose@gmail.com>
2018-05-31 20:03:30 +0400

    I did it you guys

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+
 import myname
 
 name = myname.get_name()
-print('Hello World & GIT from {}'.format(name))
+
+if __name__ == "__main__":
+	print('Hello World & GIT from {}'.format(name))
